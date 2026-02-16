6aee24f5f601b4f2b26c9a1e81afdb1eb03da275
LabComputer <lloyd.ivester@mymail.champlain.edu>
2026-02-16 15:51:00 -0500

    almost done with inner joins

--- assignment-05/sql/01-inner-joins.sql
+++ assignment-05/sql/01-inner-joins.sql
@@ -13,6 +13,16 @@
 -- -----------------------------------------------------------------------------
 -- TODO: Write your SELECT statement here
 
+SELECT
+    CONCAT(patients.first_name, ' ',  patients.last_name) AS patient_name,
+    CONCAT(doctors.first_name, ' ', doctors.last_name) AS doctor_name,
+    scheduled_at,
+    visit_type,
+    reason
+FROM appointments
+INNER JOIN patients ON appointments.patient_id = patients.patient_id
+INNER JOIN doctors ON appointments.doctor_id = doctors.doctor_id
+ORDER BY scheduled_at DESC;
 
 -- Problem 1.2 (4 points)
 -- Join with Department Information: Active doctors with their departments
@@ -23,6 +33,15 @@
 -- -----------------------------------------------------------------------------
 -- TODO: Write your SELECT statement here
 
+SELECT
+    CONCAT(doctors.first_name, ' ', doctors.last_name) AS doctor_name,
+    departments.name AS department_name,
+    CONCAT(departments.building, ' ', departments.floor) AS location, --TODO: check what is meant by location
+    doctors.hire_date
+FROM doctors
+INNER JOIN departments ON doctors.department_id = departments.department_id
+WHERE doctors.is_active = TRUE
+ORDER BY departments.name, doctors.last_name;
 
 -- Problem 1.3 (4 points)
 -- Many-to-Many Join: Doctors and their specializations
@@ -33,6 +52,15 @@
 -- -----------------------------------------------------------------------------
 -- TODO: Write your SELECT statement here
 
+SELECT
+    CONCAT(doctors.first_name, ' ', doctors.last_name) AS doctor_name,
+    specializations.name AS specialization_name,
+    doctor_specializations.is_primary,
+    doctor_specializations.certified_at AS certified_date
+FROM doctors
+INNER JOIN doctor_specializations ON doctors.doctor_id = doctor_specializations.doctor_id
+INNER JOIN specializations ON doctor_specializations.specialization_id = specializations.specialization_id
+ORDER BY doctors.last_name, doctor_specializations,is_primary, specializations.name;
 
 -- Problem 1.4 (4 points)
 -- Join with Filtering: Cardiology appointments in 2024
@@ -43,6 +71,17 @@
 -- -----------------------------------------------------------------------------
 -- TODO: Write your SELECT statement here
 
+SELECT
+    CONCAT(patients.first_name, ' ', patients.last_name) AS patient_name,
+    CONCAT(doctors.first_name, ' ', doctors.last_name) AS doctor_name,
+    appointments.scheduled_at,
+    appointments.visit_type,
+    appointments.reason
+FROM appointments
+INNER JOIN patients ON appointments.patient_id = patients.patient_id
+INNER JOIN doctors ON appointments.doctor_id = doctors.doctor_id
+INNER JOIN departments ON doctors.department_id = departments.department_id
+WHERE departments.name = 'Cardiology' AND EXTRACT(YEAR FROM appointments.scheduled_at) = 2024;
 
 -- Problem 1.5 (4 points)
 -- Multiple Joins with Aggregation: Doctor appointments by department
@@ -53,3 +92,12 @@
 -- Tip: Join all three tables, use GROUP BY doctor, then filter with HAVING
 -- -----------------------------------------------------------------------------
 -- TODO: Write your SELECT statement here
+
+SELECT
+
+FROM departments
+INNER JOIN doctors ON departments.department_id = doctors.department_id
+INNER JOIN appointments ON doctors.doctor_id = appointments.doctor_id
+GROUP BY doctors.doctor_id
+HAVING COUNT(appointments) > 5
+ORDER BY departments.name, COUNT(appointments) DESC
